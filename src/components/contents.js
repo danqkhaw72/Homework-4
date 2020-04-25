bdf2fbfe9ab133197f73782abdb8251a48dbfef4
Dang Kham <danqkhaw72@gmail.com>
2020-04-25 13:29:37 +0700

    Add data from contructor in contents.js

--- src/components/contents.js
+++ src/components/contents.js
@@ -1,17 +1,98 @@
 import React, { Component } from 'react'
 
 export class contents extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			products : [
+				{
+					name : 'Job',
+					email : '',
+					phone : '0123456789',
+					job : '',
+					labels : '',
+					status : true
+				},
+				{
+					name : 'Anna',
+					email : '',
+					phone : '01486842578',
+					job : '',
+					labels : '',
+					status : true
+				},
+				{
+					name : 'Zoe',
+					email : 'fgh@email.com',
+					phone : '01653075698',
+					job : 'Dev',
+					labels : 'DTDT,DT',
+					status : true
+				},
+				{
+					name : 'Marry',
+					email : '',
+					phone : '06842586942',
+					job : '',
+					labels : 'DTDT,DT',
+					status : true
+				},
+				{
+					name : 'Henry',
+					email : 'abc@gmail.com',
+					phone : '01659257454',
+					job : '',
+					labels : 'DTDT,DT',
+					status : true
+				},
+				{
+					name : 'Julia',
+					email : '',
+					phone : '05692482542',
+					job : '',
+					labels : '',
+					status : true
+				},
+				{
+					name : "Maria",
+					email : '',
+					phone : '0358625926',
+					job : '',
+					labels : '',
+					status : true
+				}
+			],
+			isActive : true
+		};
+	}
+
     render() {
+		
+		let elements = this.state.products.map((product, index) => {
+			let result = '';
+			if(product.status) {
+				result = <tr>
+							<th><a href="#" className="user-profile"><img src="img/user.jpg" alt=""/></a>{ product.name }</th>
+							<td>{ product.email }</td>
+							<td>{ product.phone }</td>
+							<td>{ product.job }</td>
+							<td>{ product.labels }</td> 
+						</tr>
+			}
+			return result;
+		});
+
         return (
             <section>
-    	<div class="container-fluid">
-    		<div class="row mb-5">
-    			<div class="col-xl-10 col-lg-9 col-md-8 ml-auto">
-    				<div class="row align-items-center pt-5 mt-3">
-    					<div class="col-12 mb-4 mb-xl-0 x-content">
-    						<table class="table table-hover bg-light table-responsive-sm">
+    	<div className="container-fluid">
+    		<div className="row mb-5">
+    			<div className="col-xl-10 col-lg-9 col-md-8 ml-auto">
+    				<div className="row align-items-center pt-5 mt-3">
+    					<div className="col-12 mb-4 mb-xl-0 x-content">
+    						<table className="table table-hover bg-light table-responsive-sm">
     							<thead>
-    								<tr class="text-muted">
+    								<tr className="text-muted">
     									<th>Name</th>
     									<th>Email</th>
     									<th>Phone number</th>
@@ -23,58 +104,9 @@ export class contents extends Component {
                                     <tr>
                                         <td>Contacts(7)</td>
                                     </tr>
-    								<tr>
-    									<th><a href="#" class="user-profile"><img src="img/user.jpg" alt=""/></a>Zoe</th>
-    									<td></td>
-                                        <td>0123456789</td>
-                                        <td></td>
-                                        <td></td> 
-    								</tr>
-    								<tr>
-    									<th><a href="#" class="user-profile"><img src="img/user.jpg" alt=""/></a>Catherina</th>
-    									<td></td>
-                                        <td>0123456789</td>
-                                        <td></td>
-                                        <td></td> 
-    								</tr>
-    								<tr>
-    									<th><a href="#" class="user-profile"><img src="img/user.jpg" alt=""/></a>Anna</th>
-    									<td></td>
-                                        <td>0123456789</td>
-                                        <td></td>
-                                        <td>DTDD,D</td> 
-    								</tr>
-    								<tr>
-    									<th><a href="#" class="user-profile"><img src="img/user.jpg" alt=""/></a>Marry</th>
-    									<td></td>
-                                        <td>0123456789</td>
-                                        <td></td>
-                                        <td></td> 
-    								</tr>
-    								<tr>
-    									<th><a href="#" class="user-profile"><img src="img/user.jpg" alt=""/></a>Bod</th>
-    									<td></td>
-                                        <td>0123456789</td>
-                                        <td></td>
-                                        <td></td> 
-                                    </tr>
-                                    <tr>
-    									<th><a href="#" class="user-profile"><img src="img/user.jpg" alt=""/></a>Garen</th>
-    									<td></td>
-                                        <td>0123456789</td>
-                                        <td></td>
-                                        <td></td> 
-                                    </tr>
-                                    <tr>
-    									<th><a href="#" class="user-profile"><img src="img/user.jpg" alt=""/></a>Leona</th>
-    									<td></td>
-                                        <td>0123456789</td>
-                                        <td></td>
-                                        <td></td> 
-    								</tr>
+    								{ elements }
     							</tbody>
     						</table>
-
     					</div>
     				</div>
     			</div>
--- src/components/header.js
+++ src/components/header.js
@@ -3,72 +3,70 @@ import React, { Component } from 'react'
 export class header extends Component {
     render() {
         return (
-          <nav class="navbar navbar-expand-md navbar-light">
-          <button class="navbar-toggler ml-auto mb-2 bg-light" type="button" data-toggle="collapse" data-target="#myNavbar">
-            <span class="navbar-toggler-icon"></span>
+          <nav className="navbar navbar-expand-md navbar-light">
+          <button className="navbar-toggler ml-auto mb-2 bg-light" type="button" data-toggle="collapse" data-target="#myNavbar">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="myNavbar">
-            <div class="container-fluid">
-              <div class="row">
-                
-                <div class="col-xl-2 col-lg-3 col-md-4 sidebar fixed-top">	
-                  <div class="bottom-boder pb-3 ">
-                    <button type="button" class="btn btn-round btn-light">
-                                    <span><i class="fa fa-plus text-muted fa-lg"></i></span>
-                                    <span>Create contact</span>
-                                </button>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-xl-2 col-lg-3 col-md-4 sidebar fixed-top">	
+                  <div className="bottom-boder pb-3 ">
+                    <button type="button" className="btn btn-round btn-light">
+                      <span><i className="fa fa-plus text-muted fa-lg"></i></span>
+                      <span>Create contact</span>
+                    </button>
                   </div>
-                  <ul class="navbar-nav flex-column mt-4">
-                    <li class="nav-item"><a href="#" class="nav-link text-dark p-3 mb-2 current"><i class="fas fa-user text-muted fa-lg mr-3"></i>Contacts</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-dark p-3 mb-2 sidebar-link"><i class="fas fa-history text-muted fa-lg mr-3"></i>Frequently contacted</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-dark p-3 mb-2 sidebar-link"><i class="fas fa-plus-square text-muted fa-lg mr-3"></i>Merge&fix</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-dark p-3 mb-2 sidebar-link"><i class="fas fa-chevron-up text-muted fa-lg mr-3"></i>Labels</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-dark p-3 mb-2 sidebar-link"><i class="fas fa-plus text-muted fa-lg mr-3"></i>Create label</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-dark p-3 mb-2 sidebar-link"><i class="fas fa-upload text-muted fa-lg mr-3"></i>Import</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-dark p-3 mb-2 sidebar-link"><i class="fas fa-download text-muted fa-lg mr-3"></i>Export</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-dark p-3 mb-2 sidebar-link"><i class="fas fa-print text-muted fa-lg mr-3"></i>Print</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link text-dark p-3 mb-2 sidebar-link"><i class="fas fa-caret-square-down text-muted fa-lg mr-3"></i>Other contacts</a></li>
+                  <ul className="navbar-nav flex-column mt-4">
+                    <li className="nav-item"><a href="#" className="nav-link text-dark p-3 mb-2 current"><i className="fas fa-user text-muted fa-lg mr-3"></i>Contacts</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link text-dark p-3 mb-2 sidebar-link"><i className="fas fa-history text-muted fa-lg mr-3"></i>Frequently contacted</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link text-dark p-3 mb-2 sidebar-link"><i className="fas fa-plus-square text-muted fa-lg mr-3"></i>Merge&fix</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link text-dark p-3 mb-2 sidebar-link"><i className="fas fa-chevron-up text-muted fa-lg mr-3"></i>Labels</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link text-dark p-3 mb-2 sidebar-link"><i className="fas fa-plus text-muted fa-lg mr-3"></i>Create label</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link text-dark p-3 mb-2 sidebar-link"><i className="fas fa-upload text-muted fa-lg mr-3"></i>Import</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link text-dark p-3 mb-2 sidebar-link"><i className="fas fa-download text-muted fa-lg mr-3"></i>Export</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link text-dark p-3 mb-2 sidebar-link"><i className="fas fa-print text-muted fa-lg mr-3"></i>Print</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link text-dark p-3 mb-2 sidebar-link"><i className="fas fa-caret-square-down text-muted fa-lg mr-3"></i>Other contacts</a></li>
                   </ul>
                 </div>
                 
-                <div class="col-xl-12 col-lg-12 col-md-12 ml-auto fixed-top py-2 top-navbar">
-                  <div class="row align-items-center">
-                    <div class="col-md-2 toggle">                                
-                       <a href="#" class="nav-link"><i class="fa fa-bars text-muted fa-lg"></i></a>
-                       <a href="#" class="user-profile"><img src="img/contacts.png" alt=""/><span class="size">Contacts</span></a>                               
+                <div className="col-xl-12 col-lg-12 col-md-12 ml-auto fixed-top py-2 top-navbar">
+                  <div className="row align-items-center">
+                    <div className="col-md-2 toggle">                                
+                       <a href="#" className="nav-link"><i className="fa fa-bars text-muted fa-lg"></i></a>
+                       <a href="#" className="user-profile"><img src="img/contacts.png" alt=""/><span className="size">Contacts</span></a>                               
                     </div>
-                    <div class="col-md-5">
+                    <div className="col-md-5">
                       <form>
-                        <div class="input-group">
-                          <input type="text" class="form-control search-input" placeholder="Search..."/>
-                          <button type="button" class="btn btn-light search-button"><i class="fas fa-search text-danger"></i></button>    									
+                        <div className="input-group">
+                          <input type="text" className="form-control search-input" placeholder="Search..."/>
+                          <button type="button" className="btn btn-light search-button"><i className="fas fa-search text-danger"></i></button>    									
                         </div>
                       </form>
                     </div>
-                    <div class="col-md-5 ">
-                      <ul class="navbar-nav navbar-right">
-                        <li class="nav-item icon-parent">
-                        <a href="#" class="nav-link"  data-toggle="dropdown" aria-expanded="false"><i class="fas fa-question-circle text-muted fa-lg"></i>
+                    <div className="col-md-5 ">
+                      <ul className="navbar-nav navbar-right">
+                        <li className="nav-item icon-parent">
+                        <a href="#" className="nav-link"  data-toggle="dropdown" aria-expanded="false"><i className="fas fa-question-circle text-muted fa-lg"></i>
                         </a>
-                        <div class="dropdown-menu pull-right" >
-                          <a class="dropdown-item"  href="#"> Send feedback</a>
-                            <a class="dropdown-item"  href="#"> Help</a>
+                        <div className="dropdown-menu pull-right" >
+                          <a className="dropdown-item"  href="#"> Send feedback</a>
+                            <a className="dropdown-item"  href="#"> Help</a>
                         </div>
                       </li>
-                        <li class="nav-item pl-2 icon-parent">
-                        <a href="#" class="nav-link" data-toggle="dropdown" aria-expanded="false"><i class="fas fa-cog text-muted fa-lg"></i></a>
-                        <div class="dropdown-menu pull-right" >
-                          <a class="dropdown-item"  href="#"> Undo changes</a>
-                          <a class="dropdown-item"  href="#"> More setting</a>
+                        <li className="nav-item pl-2 icon-parent">
+                        <a href="#" class="nav-link" data-toggle="dropdown" aria-expanded="false"><i className="fas fa-cog text-muted fa-lg"></i></a>
+                        <div className="dropdown-menu pull-right" >
+                          <a className="dropdown-item"  href="#"> Undo changes</a>
+                          <a className="dropdown-item"  href="#"> More setting</a>
                           </div>
                       </li>
-                          <li class="nav-item pl-4 icon-parent"><a href="#" class="nav-link"><i class="fas fa-th text-muted fa-lg"></i></a></li>
-                          <li class="nav-item pl-2"><a href="#" class="user-profile"><img src="img/user.jpg" alt=""/></a></li>
+                          <li className="nav-item pl-4 icon-parent"><a href="#" class="nav-link"><i className="fas fa-th text-muted fa-lg"></i></a></li>
+                          <li className="nav-item pl-2"><a href="#" className="user-profile"><img src="img/user.jpg" alt=""/></a></li>
                       </ul>
                     </div>
                   </div>
-                </div>
-                
+                </div> 
               </div>
             </div>
           </div>
